5ce5233f9cdf61aaf92a5f0f6ba257fd9f6dc71d
bevenky <venky@miglu.com>
2011-05-17 19:23:10 +0530

    Update comment

--- examples/example-transfercall.py
+++ examples/example-transfercall.py
@@ -12,7 +12,7 @@ AUTH_TOKEN = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
 # Create a REST object
 plivo = plivohelper.REST(REST_API_URL, SID, AUTH_TOKEN)
 
-# Hangup a call using a HTTP POST
+# Transfer a call using a HTTP POST
 transfer_call_params = {
     'URL' : "http://127.0.0.1:5000/transfered/",
     'CallUUID' : 'edaa59e1-79e0-41de-b016-f7a7570f6e9c', # Request UUID to hangup call
