e644e34680a178256371ca9cada0f340d2016372
tamiel <michael.ricordeau@gmail.com>
2011-05-20 18:05:11 +0200

    hangup example arg

--- examples/example-hangupcall.py
+++ examples/example-hangupcall.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import plivohelper
+import sys
+
+
+try:
+    call_uuid = sys.argv[1]
+except IndexError:
+    print "need CallUUID argument"
+    sys.exit(1)
 
 
 #URL of the Plivo REST Service
@@ -15,7 +23,7 @@ plivo = plivohelper.REST(REST_API_URL, SID, AUTH_TOKEN, API_VERSION)
 
 # Hangup a call using a HTTP POST
 hangup_call_params = {
-    'CallUUID' : 'edaa59e1-79e0-41de-b016-f7a7570f6e9c', # Request UUID to hangup call
+    'CallUUID' : call_uuid, # CallUUID for Hangup
 }
 
 #Perform a hangup on a Call
