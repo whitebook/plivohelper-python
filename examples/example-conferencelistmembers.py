6a2260ba44b9796ea5153837301935c768b1a9ee
tamiel <michael.ricordeau@gmail.com>
2011-07-20 11:36:05 +0200

    add all filters for example-conferencelist.py and example-conferencelistmembers.py examples

--- examples/example-conferencelistmembers.py
+++ examples/example-conferencelistmembers.py
@@ -17,6 +17,14 @@ try:
     uuid_filter = sys.argv[3]
 except IndexError:
     uuid_filter = ""
+try:
+    only_mute = sys.argv[4]
+except IndexError:
+    only_mute = 'false'
+try:
+    only_deaf = sys.argv[5]
+except IndexError:
+    only_deaf = 'false'
 
 # URL of the Plivo REST Service
 REST_API_URL = 'http://127.0.0.1:8088'
@@ -29,7 +37,8 @@ AUTH_TOKEN = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
 # Create a REST object
 plivo = plivohelper.REST(REST_API_URL, SID, AUTH_TOKEN, API_VERSION)
 
-call_params = {'ConferenceName':room, 'Members':member_filter, 'CallUUIDs':uuid_filter}
+call_params = {'ConferenceName':room, 'MemberFilter':member_filter, 
+               'CallUUIDFilter':uuid_filter, 'MutedFilter':only_mute, 'DeafFilter':only_deaf}
 
 try:
     print plivo.conference_list_members(call_params)
